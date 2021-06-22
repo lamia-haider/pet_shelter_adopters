7bdc6b938c3b645df5d590ce6d1fe291c0c37385
Lamia Haider <lamia.haider@gmail.com>
2021-06-22 05:42:12 -0400

    Added skeleton of scope methods in user model to group pets by species

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,9 @@ class User < ApplicationRecord
   validates_confirmation_of :password, :on=>:create
   validates_length_of :password, :within => Devise.password_length 
 
-  scope :pets_interested, -> {joins(:interested).group('pet.name')}
-  
+  scope :pets_interested, -> {joins(:interested).group('users.name')}
+  scope :interested_cats, -> {joins(:interested).where('pets.species = cat').group('users.name')
+  scope :interested_dogs, -> {joins(:interested).where('pets.species = dog').group('users.name')
 
   def self.from_omniauth(access_token)
     data = access_token.info
