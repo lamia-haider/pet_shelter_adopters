88e13deea96e65a55938f429fe6c785f816e212f
Lamia Haider <lamia.haider@gmail.com>
2021-06-22 03:32:58 -0400

    Basic _pet form page created

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
  
  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resource :pets
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
