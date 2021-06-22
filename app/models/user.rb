b4c4454f0d4ccbaa74bb67c24cdadc26d2d2ce19
Lamia Haider <lamia.haider@gmail.com>
2021-06-22 03:06:27 -0400

    Added views for users, pets and basic home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,19 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
-  :database_authenticatable, :registerable,
+  # :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :validatable
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+  
+    unless user
+      password = Devise.friendly_token[0,20]
+      user = User.create(name: data["name"], email: data["email"],
+        password: password, password_confirmation: password
+      )
+    end
+    user
+  end
 end
