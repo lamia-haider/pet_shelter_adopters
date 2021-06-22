8a0d42487286ce7108f1f12886dff0e77de8e9f7
Lamia Haider <lamia.haider@gmail.com>
2021-06-22 03:53:08 -0400

    updated layout with flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
  
  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resource :pets
+  resources :pets
+  resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
