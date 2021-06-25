396f603a8f75aeefb0af7b24b571c1a860bc7a15
Lamia Haider <lamia.haider@gmail.com>
2021-06-25 16:10:52 -0400

    Added css and html for pet and user views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
 
     def index
         @pets = Pet.all
-        @interested_all = Interested.all
+        @pet = []
+        @interested_user = []
+        @interested_user = Interested.select do |interested| interested.user_id == current_user.id
+            @interested_user.each do |int|
+                @pet << int.pet_id 
+            
+            end
+
+        end
+ 
+        
     end
 end
